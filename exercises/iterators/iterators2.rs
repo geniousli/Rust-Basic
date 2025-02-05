2fb8772a282c516f14791ade39e48a7e5dbabaf5
shaohua.li <wildimagine@qq.com>
2025-02-06 00:13:50 +0800

    [done]:

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,25 +6,33 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
-    match c.next() {
+    let mut first = match c.next() {
         None => String::new(),
-        Some(first) => ???,
-    }
-}
+        Some(first) => first.to_uppercase().to_string(),
+    };
 
+    while let Some(ch) = c.next() {
+        first.push(ch);
+    }
+    first
+}
 // Step 2.
 // Apply the `capitalize_first` function to a slice of string slices.
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut res = Vec::default();
+    for w in words {
+        let s = capitalize_first(w);
+        res.push(s);
+    }
+    res
 }
 
 // Step 3.
@@ -32,7 +40,12 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut res = String::default();
+    for w in words {
+        let s = capitalize_first(w);
+        res.push_str(&s);
+    }
+    res
 }
 
 #[cfg(test)]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,23 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let n_s = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(size) => {
+                    let mut istring = string.clone();
+                    for i in 0..*size {
+                        istring.push_str("bar");
+                    }
+                    istring.to_string()
+                }
+            };
+            output.push(n_s);
         }
         output
     }
@@ -45,8 +55,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
